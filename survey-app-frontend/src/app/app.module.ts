46ad57dd66e71212f0150b55e9ea2350ad96daeb
maharshikahodariyadaiict <maharshi.2021@gmail.com>
2019-09-27 19:18:36 +0530

    Routing set in frontend mk_1

--- survey-app-frontend/src/app/app.module.ts
+++ survey-app-frontend/src/app/app.module.ts
@@ -3,14 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateSurveyComponent } from './create-survey/create-survey.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+const materialModules = [
+  MatToolbarModule,
+  MatIconModule
+]
+
+const appRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'createSurvey', component: CreateSurveyComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CreateSurveyComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    materialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
