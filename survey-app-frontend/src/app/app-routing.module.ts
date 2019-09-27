daaeed8f5513e80fabf8c4e90aed97be4bfc1735
maharshikahodariyadaiict <maharshi.2021@gmail.com>
2019-09-27 20:16:45 +0530

    Created skeleton for frontend mk_2

--- survey-app-frontend/src/app/app-routing.module.ts
+++ survey-app-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateSurveyComponent } from './create-survey/create-survey.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'createSurvey', component: CreateSurveyComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
